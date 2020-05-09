9b6309ddf1fd54f47737e6ae2dd7967844c2c61f
Vitor Zamprogno Amancio Pereira <vitor.zapereira@gmail.com>
2020-05-09 15:39:07 -0300

    src/config/database.js points to heroku postgres server

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'eQueue',
+  dialectOptions: {
+    ssl: true
+  },
+  host: 'ec2-54-217-204-34.eu-west-1.compute.amazonaws.com',
+  username: 'msxkivqjdxdbao',
+  password: 'dd9d8af288ea982cc3d2a67dc8018e07fee4426ae0f095944af127eddf7e8b0f',
+  database: 'd81sm4vf6l7fc1',
   define: {
     timestamp: true,
     underscored: true,
@@ -11,3 +14,16 @@ module.exports = {
     freezeTableName: false
   }
 };
+// module.exports = {
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   username: 'postgres',
+//   password: 'docker',
+//   database: 'eQueue',
+//   define: {
+//     timestamp: true,
+//     underscored: true,
+//     underscoredAll: true,
+//     freezeTableName: false
+//   }
+// };
