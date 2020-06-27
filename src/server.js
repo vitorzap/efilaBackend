bd1a866194a7bf5dbd2eea6bed7369fb763bd9df
Vitor Zamprogno Amancio Pereira <vitor.zapereira@gmail.com>
2020-06-27 14:30:15 -0300

    server .js deleted - It is no longer necessary

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-app.on('connection', socket => {
-  console.log(`Socket conectado: ${socket.id}`);
-});
-
-const port = process.env.PORT || 3333;
-
-app.listen(port, () => console.log(`Server port = (${port})`));
